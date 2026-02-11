bb3147c9e3e8ff5dc57ad4c7e2a44afb56c0b993
ViihByte <fariasvitoria08@gmail.com>
2026-02-11 15:23:55 -0300

    descrição
    adicionei a descrição das funcionalidades

--- teste1/Testes/src/estudos/java/estruturado/repeticoes/ex_for.java
+++ teste1/Testes/src/estudos/java/estruturado/repeticoes/ex_for.java
@@ -2,6 +2,7 @@ package estudos.java.estruturado.repeticoes;
 
 public class ex_for {
     public static void main(String[] args) {
+        //for → Quando você SABE quantas vezes vai repetir
         int count = 10;
         for(int i=0; i < count ; i++){
             System.out.println(i);
--- teste1/Testes/src/estudos/java/estruturado/repeticoes/ex_while.java
+++ teste1/Testes/src/estudos/java/estruturado/repeticoes/ex_while.java
@@ -2,7 +2,7 @@ package estudos.java.estruturado.repeticoes;
 
 public class ex_while {
     public static void main(String[] args) {
-
+        //while → Quando você não sabe exatamente quantas vezes vai repetir
         int count = 0;
         while (count < 10){
             System.out.println(++count);
