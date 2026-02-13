eb3059108110abb7b65fd37658d2cb0bf20946c8
ViihByte <fariasvitoria08@gmail.com>
2026-02-13 10:28:10 -0300

    novos conteudos

---
+++ teste1/Testes/src/estudos/java/estruturado/arrays/ex_Foreache1.java
@@ -0,0 +1,11 @@
+package estudos.java.estruturado.arrays;
+
+public class ex_Foreache1 {
+    public static void main(String[] args) {
+        String[] nomes = {"Vicky", "Freed", "Huck", "Lesce"};
+
+      for(String i : nomes){
+          System.out.println(i);
+      }
+    }
+}
---
+++ teste1/Testes/src/estudos/java/estruturado/arraysMultidimensionais/ex1.java
@@ -0,0 +1,7 @@
+package estudos.java.estruturado.arraysMultidimensionais;
+
+public class ex1 {
+    public static void main(String[] args) {
+        
+    }
+}
