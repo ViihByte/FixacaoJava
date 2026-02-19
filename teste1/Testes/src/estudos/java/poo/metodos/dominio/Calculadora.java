608b61c8099222b12719cc6f8d75d79947bb5ead
ViihByte <fariasvitoria08@gmail.com>
2026-02-19 12:19:09 -0300

    Introdução a Metodos

---
+++ teste1/Testes/src/estudos/java/poo/metodos/dominio/Calculadora.java
@@ -0,0 +1,8 @@
+package estudos.java.poo.metodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        System.out.println("Soma: "+ (10+10));
+    }
+}
---
+++ teste1/Testes/src/estudos/java/poo/metodos/test/UsaClassesCalculadora.java
@@ -0,0 +1,10 @@
+package estudos.java.poo.metodos.test;
+
+import estudos.java.poo.metodos.dominio.Calculadora;
+
+public class UsaClassesCalculadora {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+    }
+}
