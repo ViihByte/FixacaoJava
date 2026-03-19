36016144a94f75cd6a97bad245d40435fea12e07
ViihByte <fariasvitoria08@gmail.com>
2026-03-19 15:53:11 -0300

    Blocos de inicialização

---
+++ teste1/Testes/src/estudos/java/poo/blocosDeInicializacao/dominio/Anime.java
@@ -0,0 +1,37 @@
+package estudos.java.poo.blocosDeInicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+
+    {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+
+    public Anime() {
+        for(int episodios:this.episodios){
+            System.out.print(episodios + "; ");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(int[] episodios) {
+        this.episodios = episodios;
+    }
+}
---
+++ teste1/Testes/src/estudos/java/poo/blocosDeInicializacao/test/UsaClassesAnime.java
@@ -0,0 +1,10 @@
+package estudos.java.poo.blocosDeInicializacao.test;
+
+import estudos.java.poo.blocosDeInicializacao.dominio.Anime;
+
+public class UsaClassesAnime {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+
+    }
+}
