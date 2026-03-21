7647b8151bd23ccd485731856704896eccfeb503
ViihByte <fariasvitoria08@gmail.com>
2026-03-21 15:23:27 -0300

    Blocos de inicalização static

---
+++ teste1/Testes/src/estudos/java/poo/modificadorEstatico/dominio/Anime.java
@@ -0,0 +1,34 @@
+package estudos.java.poo.modificadorEstatico.dominio;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    static {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+
+    public Anime() {
+        for(int episodios:Anime.episodios){
+            System.out.print(episodios + "; ");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+}
---
+++ teste1/Testes/src/estudos/java/poo/modificadorEstatico/test/AnimeTest.java
@@ -0,0 +1,15 @@
+package estudos.java.poo.modificadorEstatico.test;
+
+import estudos.java.poo.modificadorEstatico.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+
+    }
+
+
+}
